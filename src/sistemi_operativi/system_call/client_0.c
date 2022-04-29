61203c614315b53c80956d591fc3b3bafe188be2
Enrico Bonoldi <37395390+bonoldie@users.noreply.github.com>
2022-04-29 13:55:26 +0000

    header files

--- src/sistemi_operativi/system_call/client_0.c
+++ src/sistemi_operativi/system_call/client_0.c
@@ -1,17 +1,24 @@
 /// @file client.c
 /// @brief Contiene l'implementazione del client.
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "defines.h"
 
 int main(int argc, char * argv[]) {
-  sigset_t mySet, prevSet;
+ /* sigset_t mySet, prevSet;
 
   sigfillset(&mySet);
 
   sigdelset(&mySet, SIGUSR1);
   sigdelset(&mySet, SIGINT);
 
-  sigprocmask(SIG_SETMASK, &mySet, &prevSet);
+  sigprocmask(SIG_SETMASK, &mySet, &prevSet); */
 
     return 0;
 }
--- src/sistemi_operativi/system_call/server.c
+++ src/sistemi_operativi/system_call/server.c
@@ -1,15 +1,25 @@
 /// @file sender_manager.c
 /// @brief Contiene l'implementazione del sender_manager.
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
 #include "err_exit.h"
 #include "defines.h"
 #include "shared_memory.h"
 #include "semaphore.h"
 #include "fifo.h"
 
+
+
 int main(int argc, char * argv[]) {
 
-  int size = 50;
+  /*int size = 50;
 
   if(mkfifo("/src/sistemi_operativi", S_IRUSR|S_IWUSR) == -1)
     errExit("non sono riuscito a creare la fifo1");
@@ -17,6 +27,7 @@ int main(int argc, char * argv[]) {
  // if(mkfifo("/src/sistemi_operativi", S_IRUSR|S_IWUSR) == -1)
   //  errExit("non sono riuscito a creare la fifo2");
   //non so se per fare una seconda fifo devo mettere un pathname diverso dalla fifo1
+  // sigset_t ??
 
   int shdm = shmget(IPC_PRIVATE, size, IPC_CREAT | S_IRUSR | S_IWUSR);
 
@@ -25,7 +36,7 @@ int main(int argc, char * argv[]) {
   int sem = semget(PIC_PRIVATE, 5,  IPC_CREAT | S_IRUSR | S_IWUSR);
 
   int fd = open("/src/sistemi_operativi", O_RDONLY);
-
+*/
   
     return 0;
 }
